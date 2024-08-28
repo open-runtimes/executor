b7d8c638daf52568abb187876951afb54eff723a
Christy Jacob <christyjacob4@gmail.com>
2024-08-28 12:09:45 +0400

    chore: remove unexpected files

--- tests/resources/functions/php-fn/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return function ($request, $response) {
-    $response->json([
-        'one' => $request->env['one'],
-        'two' => $request->env['two'],
-        'three' => $request->env['three']
-    ]);
-};
