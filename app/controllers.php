47340bc3d500068271648c2a7361c6fec7ae29a4
loks0n <22452787+loks0n@users.noreply.github.com>
2025-08-05 11:44:07 +0100

    feat: scope errors

--- app/controllers.php
+++ app/controllers.php
@@ -4,9 +4,7 @@ require_once __DIR__ . '/init.php';
 
 use OpenRuntimes\Executor\BodyMultipart;
 use OpenRuntimes\Executor\Runner\Adapter as Runner;
-use Utopia\CLI\Console;
 use Utopia\Logger\Log;
-use Utopia\Logger\Logger;
 use Utopia\System\System;
 use Utopia\Http\Http;
 use Utopia\Http\Request;
@@ -19,32 +17,6 @@ use Utopia\Http\Validator\Integer;
 use Utopia\Http\Validator\Text;
 use Utopia\Http\Validator\WhiteList;
 use Utopia\Orchestration\Adapter\DockerAPI;
-
-function logError(Log $log, Throwable $error, string $action, Logger $logger = null): void
-{
-    Console::error('[Error] Type: ' . get_class($error));
-    Console::error('[Error] Message: ' . $error->getMessage());
-    Console::error('[Error] File: ' . $error->getFile());
-    Console::error('[Error] Line: ' . $error->getLine());
-
-    if ($logger && ($error->getCode() === 500 || $error->getCode() === 0)) {
-        $log->setType(Log::TYPE_ERROR);
-        $log->setMessage($error->getMessage());
-        $log->setAction($action);
-
-        $log->addTag('code', \strval($error->getCode()));
-        $log->addTag('verboseType', get_class($error));
-
-        $log->addExtra('file', $error->getFile());
-        $log->addExtra('line', $error->getLine());
-        $log->addExtra('trace', $error->getTraceAsString());
-        // TODO: @Meldiron Uncomment, was warning: Undefined array key "file" in Sentry.php on line 68
-        // $log->addExtra('detailedTrace', $error->getTrace());
-
-        $responseCode = $logger->addLog($log);
-        Console::info('Executor log pushed with status code: ' . $responseCode);
-    }
-}
 
 Http::get('/v1/runtimes/:runtimeId/logs')
     ->groups(['api'])
@@ -338,66 +310,6 @@ Http::get('/v1/health')
             'runtimes' => $stats->getContainerUsage(),
         ];
         $response->setStatusCode(Response::STATUS_CODE_OK)->json($output);
-    });
-
-/** Set callbacks */
-Http::error()
-    ->inject('error')
-    ->inject('logger')
-    ->inject('response')
-    ->inject('log')
-    ->action(function (Throwable $error, ?Logger $logger, Response $response, Log $log) {
-        try {
-            logError($log, $error, "httpError", $logger);
-        } catch (Throwable) {
-            Console::warning('Unable to send log message');
-        }
-
-        $version = Http::getEnv('OPR_EXECUTOR_VERSION', 'UNKNOWN');
-        $message = $error->getMessage();
-        $file = $error->getFile();
-        $line = $error->getLine();
-        $trace = $error->getTrace();
-
-        switch ($error->getCode()) {
-            case 400: // Error allowed publicly
-            case 401: // Error allowed publicly
-            case 402: // Error allowed publicly
-            case 403: // Error allowed publicly
-            case 404: // Error allowed publicly
-            case 406: // Error allowed publicly
-            case 409: // Error allowed publicly
-            case 412: // Error allowed publicly
-            case 425: // Error allowed publicly
-            case 429: // Error allowed publicly
-            case 501: // Error allowed publicly
-            case 503: // Error allowed publicly
-                $code = $error->getCode();
-                break;
-            default:
-                $code = 500; // All other errors get the generic 500 server error status code
-        }
-
-        $output = Http::isDevelopment() ? [
-            'message' => $message,
-            'code' => $code,
-            'file' => $file,
-            'line' => $line,
-            'trace' => \json_encode($trace, JSON_UNESCAPED_UNICODE) === false ? [] : $trace, // check for failing encode
-            'version' => $version
-        ] : [
-            'message' => $message,
-            'code' => $code,
-            'version' => $version
-        ];
-
-        $response
-            ->addHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-            ->addHeader('Expires', '0')
-            ->addHeader('Pragma', 'no-cache')
-            ->setStatusCode($code);
-
-        $response->json($output);
     });
 
 Http::init()
