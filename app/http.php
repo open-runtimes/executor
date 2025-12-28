9427aad9b66ee03ed99ecf6f3edf8a65942d0461
loks0n <22452787+loks0n@users.noreply.github.com>
2025-12-28 18:46:06 +0000

    refactor: network manager

--- app/http.php
+++ app/http.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/controllers.php';
 
 use OpenRuntimes\Executor\Runner\Docker;
 use OpenRuntimes\Executor\Runner\Repository\Runtimes;
+use OpenRuntimes\Executor\Runner\NetworkManager;
 use Swoole\Runtime;
 use Utopia\Console;
 use Utopia\Http\Http;
@@ -23,6 +24,12 @@ use function Swoole\Coroutine\run;
 // Unlimited memory limit to handle as many coroutines/requests as possible
 ini_set('memory_limit', '-1');
 
+$payloadSize = 22 * (1024 * 1024);
+$settings = [
+    'package_max_length' => $payloadSize,
+    'buffer_output_size' => $payloadSize,
+];
+
 Runtime::enableCoroutine(true, SWOOLE_HOOK_ALL);
 
 Http::setMode((string)System::getEnv('OPR_EXECUTOR_ENV', Http::MODE_TYPE_PRODUCTION));
@@ -33,22 +40,25 @@ Http::onRequest()
         $response->addHeader('Server', 'Executor');
     });
 
-
-run(function () {
+run(function () use ($settings) {
     $orchestration = new Orchestration(new DockerAPI(
         System::getEnv('OPR_EXECUTOR_DOCKER_HUB_USERNAME', ''),
         System::getEnv('OPR_EXECUTOR_DOCKER_HUB_PASSWORD', '')
     ));
+
+    /* Create desired networks if they don't exist */
     $networks = explode(',', System::getEnv('OPR_EXECUTOR_NETWORK') ?: 'openruntimes-runtimes');
-    $runner = new Docker($orchestration, new Runtimes(), $networks);
+    $networkManager = new NetworkManager($orchestration, $networks);
 
+    /* Add the current executor to the networks */
+    $hostname = gethostname() ?: throw new \RuntimeException('Could not determine hostname');
+    $selfContainer = $orchestration->list(['name' => $hostname])[0] ?? throw new \RuntimeException('Own container not found');
+    $networkManager->connectAll($selfContainer);
+
+    /* Runner service, used to manage runtimes */
+    $runtimes = new Runtimes();
+    $runner = new Docker($orchestration, $runtimes, $networkManager);
     Http::setResource('runner', fn () => $runner);
-
-    $payloadSize = 22 * (1024 * 1024);
-    $settings = [
-        'package_max_length' => $payloadSize,
-        'buffer_output_size' => $payloadSize,
-    ];
 
     $server = new Server('0.0.0.0', '80', $settings);
     $http = new Http($server, 'UTC');
