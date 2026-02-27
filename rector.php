a5edbb72399f56c0d8b5004acfe743c7736477fd
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-02-27 17:53:13 +0530

    disable rule

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Privatization\Rector\Class_\FinalizeTestCaseClassRector;
+use Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,6 +15,7 @@ return RectorConfig::configure()
     ->withSkipPath(__DIR__ . '/tests/resources')
     ->withSkip([
         FinalizeTestCaseClassRector::class,
+        ThrowWithPreviousExceptionRector::class,
     ])
     ->withPhpSets(php84: true)
     ->withComposerBased(phpunit: true)
